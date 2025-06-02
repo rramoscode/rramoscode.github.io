7a0dedc4719d5d0d332f11c82a58b0bf6c764ed7
rramoscode <randy.ramos@snhu.edu>
2025-06-01 21:28:54 -0500

    Add files via upload
    Enhancement One was made to DataDisplayActvity.java file as part of Software Engineering and Design.

---
+++ NuMe_Enhancement_1/app/src/main/java/com/zybooks/numerandy/DatabaseHelper.java
@@ -0,0 +1,36 @@
+package com.zybooks.numerandy;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "weight_tracker.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //create the users table with username and password columns
+        db.execSQL("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)");
+
+        // create weight tracking table
+        db.execSQL("CREATE TABLE daily_weights (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, date TEXT, weight REAL)");
+
+        // create a table to store the goal weight
+        db.execSQL("CREATE TABLE goal_weight (id INTEGER  PRIMARY KEY AUTOINCREMENT, user_id INTEGER, goal_weight REAL)");
+
+        // add new goal_history table ()
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS users");
+        db.execSQL("DROP TABLE IF EXISTS daily_weights");
+        onCreate(db);
+    }
+}
